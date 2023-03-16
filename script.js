a2b6f4f07cc6fc625bbf8b71412678a8ee86cddc
Marco <103689125+marcoserrano21@users.noreply.github.com>
2023-03-16 16:33:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,47 +3,22 @@ const apiKey = '25e4eb518cb9ad425f16a3669d0b4ede'; // Replace with your own Open
 const input = document.querySelector('input[type="text"]');
 const button = document.querySelector('button');
 const cityEl = document.querySelector('.city');
-const tempEl = document.querySelector('.temperature');
+const tempEl = document.querySelector('.temp');
 const descEl = document.querySelector('.description');
 const celsiusEl = document.querySelector('#celsius');
 const fahrenheitEl = document.querySelector('#fahrenheit');
 
-if (!input) {
-  console.error('Could not find input element');
-}
-
-if (!button) {
-  console.error('Could not find button element');
-}
-
-if (!cityEl) {
-  console.error('Could not find city element');
-}
-
-if (!tempEl) {
-  console.error('Could not find temperature element');
-}
-
-if (!descEl) {
-  console.error('Could not find description element');
-}
-
-if (!celsiusEl) {
-  console.error('Could not find celsius element');
-}
-
-if (!fahrenheitEl) {
-  console.error('Could not find fahrenheit element');
-}
+let originalTemp = 0; // Store the original temperature value
 
 button.addEventListener('click', () => {
-  const city = input.value ? input.value.trim() : '';
+  const city = input.value.trim();
   if (city) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
       .then(response => response.json())
       .then(data => {
         cityEl.textContent = data.name;
-        tempEl.textContent = `${Math.round(data.main.temp)}°`;
+        originalTemp = Math.round(data.main.temp); // Update the original temperature value
+        tempEl.textContent = `${originalTemp}°`;
         descEl.textContent = data.weather[0].description;
       })
       .catch(error => console.error(error));
@@ -55,13 +30,13 @@ button.addEventListener('click', () => {
 celsiusEl.addEventListener('click', () => {
   const temp = parseInt(tempEl.textContent);
   if (!isNaN(temp)) {
-    tempEl.textContent = `${temp}°`;
+    tempEl.textContent = `${originalTemp}°`; // Use the original temperature value to convert to Celsius
   }
 });
 
 fahrenheitEl.addEventListener('click', () => {
   const temp = parseInt(tempEl.textContent);
   if (!isNaN(temp)) {
-    tempEl.textContent = `${Math.round(temp * 1.8 + 32)}°`;
+    tempEl.textContent = `${Math.round(originalTemp * 1.8 + 32)}°`; // Use the original temperature value to convert to Fahrenheit
   }
 });
